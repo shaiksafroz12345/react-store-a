98c972f8f16f8f4d8eeaa44e2d34c9f243b25a2c
shaiksafroz12345 <shaikafroz2k4@gmail.com>
2025-03-18 14:35:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import "./App.css";
 import Header from "./components/Header";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
-import {BrowserRouter,Route,Routes} from "react-router"
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Header />
-      <Products />
-      <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Products />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import "./Header.css"
-export default function Header(){
-    return(
-        <div className="App-Header-Row">
-            <div>My React Store</div>
-            <div>Menu</div>
-        </div>
-    )
+import "./Header.css";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <div className="App-Header-Row">
+      <h1>My React Store</h1>
+      <div>
+        <Link to="products">Home</Link>
+        <Link to="login">Login</Link>
+      </div>
+    </div>
+  );
 }
